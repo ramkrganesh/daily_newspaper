6592114636ead28740e8fa693e4d4fc6b3420b25
rravich <ramkrganesh@gmail.com>
2026-06-27 13:26:33 +0200

    added a "json" file to configure rss feed sources.

--- src/news_aggregator.py
+++ src/news_aggregator.py
@@ -1,2 +1,14 @@
+import feedparser
+import json
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+def get_json_cfg()->dict:
+    cfg = None  # a dict
+    with open(Path(r"..\rss_cfg.json").resolve(), 'r') as fp:
+        cfg = json.load(fp)
+    return cfg
+
 if __name__=='__main__':
+    rss_cfg = get_json_cfg()
     print('Starting Newspaper aggregator...')
