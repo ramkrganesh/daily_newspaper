115c4c2aaafdfed116b96bbbe0ec432494878cfd
rravich <ramkrganesh@gmail.com>
2026-06-30 21:25:14 +0200

    starting with python script

--- src/news_aggregator.py
+++ src/news_aggregator.py
@@ -9,6 +9,18 @@ def get_json_cfg()->dict:
         cfg = json.load(fp)
     return cfg
 
+def get_headlines_articles(_rss_cfg:dict)->tuple[str,str]:
+    headlines = []
+    articles = []
+    for section, news_entities in _rss_cfg.items():
+        for source, feed_link in news_entities.items():
+            feed = feedparser.parse(feed_link)
+            for entry in feed.entries[:5]:
+                headlines.append(entry.title_detail.value)
+                print(entry.title_detail.value)
+#end def
+
 if __name__=='__main__':
     rss_cfg = get_json_cfg()
+    (headlines, articles) = get_headlines_articles(rss_cfg)
     print('Starting Newspaper aggregator...')
